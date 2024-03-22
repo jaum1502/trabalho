5a7385bcf5204a430d41fdfbdcee5d1fad6a2d4e
jaum1502 <aquelecaraalto@gmail.com>
2024-03-22 18:02:30 -0300

    mais uma

--- src/App13.java
+++ src/App13.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class App13 {
     public App13(){
+        Scanner entrada = new Scanner(System.in);
         System.out.println("Digite o comprimento da parede: ");
-        System.out.println("Digite o largura da parede: ");
+        float comp = entrada.nextFloat();
+        System.out.println("Digite o altura da parede: ");
+        float alt = entrada.nextFloat();
+        System.out.println("Digite o valor gasto: ");
+        float valorGasto = entrada.nextFloat();
+        float area = comp*alt;
+        double valor= valorGasto/area;
+        double valor2 = valor*112.50;
+        System.out.println(valor);
+        entrada.close();
     }
     }
 
