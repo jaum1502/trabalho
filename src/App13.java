9a79f438ad34cd33c56f83e39053d33ae99641e1
jaum1502 <aquelecaraalto@gmail.com>
2024-03-23 08:23:14 -0300

    atualizacao 08:23

--- src/App13.java
+++ src/App13.java
@@ -1,19 +1,24 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class App13 {
     public App13(){
         Scanner entrada = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("0.00");
         System.out.println("Digite o comprimento da parede: ");
         float comp = entrada.nextFloat();
         System.out.println("Digite o altura da parede: ");
         float alt = entrada.nextFloat();
-        System.out.println("Digite o valor gasto: ");
-        float valorGasto = entrada.nextFloat();
-        float area = comp*alt;
-        double valor= valorGasto/area;
-        double valor2 = valor*112.50;
-        System.out.println(valor);
+        int mq = 9;
+        double vmq = 12.50;
+        double area = comp*alt;
+        double valor = area*mq;
+        double valorFinal = valor*vmq;
+        System.out.println("O total deu R$" + df.format(valorFinal));
         entrada.close();
+    }
+    public static void main(String[] args) {
+        new App13();
     }
     }
 
