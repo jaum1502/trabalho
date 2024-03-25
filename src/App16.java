c250c1abd37cca8fe27d8374ebef2d491e0270dc
jaum1502 <158088928+jaum1502@users.noreply.github.com>
2024-03-25 07:52:11 -0300

    Update App16.java

--- src/App16.java
+++ src/App16.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class App16 {
     public App16() {
-        //-------------------------------------------------------------ENTRADAtesteteste------------------------------------------------------------------------
+        //-------------------------------------------------------------ENTRADA------------------------------------------------------------------------
         Scanner entrada = new Scanner(System.in);
         System.out.println("Digite o primeiro número:");
         int valor1 = entrada.nextInt();
